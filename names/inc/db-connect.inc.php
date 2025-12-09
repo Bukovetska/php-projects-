6bfcda25b9453ec983ebb5537ec8385c73972640
Bukovetska <169065797+Bukovetska@users.noreply.github.com>
2025-12-10 00:32:09 +0200

    Add files via upload

---
+++ names/inc/db-connect.inc.php
@@ -0,0 +1,12 @@
+<?php
+
+try {
+    $pdo = new PDO('mysql:host=localhost;dbname=names;charset=utf8mb4', 'names', 'k/CG@c[@*JqNkICm', [
+        PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
+    ]);
+}
+catch (PDOException $e) {
+    // var_dump($e->getMessage());
+    echo 'A problem occured with the database connection...';
+    die();
+}
